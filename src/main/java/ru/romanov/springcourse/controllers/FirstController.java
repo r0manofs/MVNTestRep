027c177527dc36e9b3460293c810c9a7b6698a24
Andrew <r0manofs@yandex.ru>
2023-02-09 13:55:36 +0300

    Add simple calculator

--- src/main/java/ru/romanov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/romanov/springcourse/controllers/FirstController.java
@@ -1,17 +1,46 @@
 package ru.romanov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message", name + " " + surname);
+
         return "first/hello";
     }
+
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+
+        double answer = switch (action) {
+            case "addition" -> a + b;
+            case "subtraction" -> a - b;
+            case "multiplication" -> a * b;
+            case "division" -> (double)a / b;
+            default -> -1;
+        };
+
+        model.addAttribute("message",
+                a + " " + action + " " + b + " = " + answer);
+
+        return "first/calculator";
     }
 }
